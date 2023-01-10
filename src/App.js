06aa87033d61538a4d40d8ceec9ec38fdbe79995
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-10 19:23:39 +0200

    Added a bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        <div>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
           Your Todo's
         </div>
-        <div>
-          <table>
+        <div className='card-body'>
+          <table className='table table-hover'>
             <thead>
               <tr>
                 <th scope='col'>#</th>
